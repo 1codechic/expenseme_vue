7035fb48172e327057f349a7a46a65fe18577cfd
1codechic <1codechic@devchics.com>
2019-04-27 17:25:52 -0400

    moved Header, Login, Signup from components to views, updated router, added Expenses.vue

--- src/router.js
+++ src/router.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Login from '@/components/Login.vue'
-import Signup from '@/components/Signup.vue'
-
+// import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import Signup from './views/Signup.vue'
 Vue.use(Router)
 
 export default new Router({
